c7df445f778887917c0916fe67fb85140cfaa14b
Ryan <ryansheikh003@gmail.com>
2024-05-31 12:42:02 -0600

    Implemented front end
    Header
    
    Home page

---
+++ app/components/Header.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import Image from 'next/image'
+const Header = () => {
+  return (
+            <header className="bg-white">
+              <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8 border-b">
+                <div className="flex h-16 items-center justify-between">
+                  <div className="flex-1 md:flex md:items-center md:gap-12">
+                    <Image src='/logo.svg' width={140} height={100}  />
+                  </div>
+
+                  <div className="md:flex md:items-center md:gap-12">
+                    <nav aria-label="Global" className="hidden md:block">
+                      <ul className="flex items-center gap-6 text-sm">
+                        <li>
+                          <a className="text-gray-500 transition hover:text-gray-500/75" href="#"> Home </a>
+                        </li>
+
+                        <li>
+                          <a className="text-gray-500 transition hover:text-gray-500/75" href="#">Upload</a>
+                        </li>
+
+                        <li>
+                          <a className="text-gray-500 transition hover:text-gray-500/75" href="#"> About us </a>
+                        </li>
+
+                        <li>
+                          <a className="text-gray-500 transition hover:text-gray-500/75" href="#"> Contact us </a>
+                        </li>
+                      </ul>
+                    </nav>
+
+                    <div className="flex items-center gap-4">
+                      <div className="sm:flex sm:gap-4">
+                        <a
+                          className="rounded-md bg-primary px-5 py-2.5 text-sm font-medium text-white shadow transition hover:bg-blue-700"
+                          href="#"
+                        >
+                          Get Started
+                        </a>
+                      </div>
+
+                      <div className="block md:hidden">
+                        <button className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-5 w-5"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                          >
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+                          </svg>
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </header>
+  )
+}
+
+export default Header
---
+++ app/utils/Constant.js
@@ -0,0 +1,3 @@
+export default{
+    desc: 'Drag and drop your on our cloud and share it with your friends securely with password and send it on email'
+}
