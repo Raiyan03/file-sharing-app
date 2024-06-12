92411f4293f7cbe5cde65cad1f3b93ea98941228
Ryan <ryansheikh003@gmail.com>
2024-06-12 14:07:19 -0600

    Bug fix toast message

--- app/(dashboard)/(routes)/file-preview/components/FileForm.jsx
+++ app/(dashboard)/(routes)/file-preview/components/FileForm.jsx
@@ -30,7 +30,12 @@ const FileForm = ({file, savePassword}) => {
             shortUrl: file.shortUrl
         }
         GlobalApi.SendEmail(data).then(res => {
-            res.statusText == "OK" ? toast.success('Email sent successfully') : toast.error('Error sending email')
+            console.log(res);
+           if (res.status == 200){
+               toast.success('Email sent successfully')
+           } else{
+                toast.error('Error sending email')
+           }
         });
     }
 
